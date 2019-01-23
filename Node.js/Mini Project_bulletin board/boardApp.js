b5c181aaec53d9427080ce05df1842ad31130739
jooyeong <young951109@naver.com>
2019-01-23 21:56:27 +0900

    boardApp.js - 첨부파일 파일정보 받아온 후 파일명 출력하기

--- Node.js/Mini Project_bulletin board/boardApp.js
+++ Node.js/Mini Project_bulletin board/boardApp.js
@@ -7,6 +7,10 @@ var static = require('serve-static');
 var path = require('path');
 var session = require('express-session');
 
+//파일 업로드용를 위해 사용되는 multipart/form-data를 다루기 위한 node.js 미들웨어
+var multer = require('multer');
+var fs = require('fs');
+
 var app = express();
 
 //port 설정
@@ -19,6 +23,7 @@ app.use(bodyParser.urlencoded({
 }))
 
 app.use('/form', static(path.join(__dirname, 'form')));
+app.use('/upload', static(path.join(__dirname, 'upload')));
 
 //세션 생성
 app.use(session({
@@ -26,6 +31,18 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }));
+
+// var upload = multer({
+//   storage: storage,
+//   limits: { //파일 제한 10개, 1G (= 2^30)
+//     files: 10,
+//     fileSize: 1024 * 1024 * 1024;
+//   }
+// });
+
+var upload = multer({
+  destination: 'upload'
+});
 
 //라우터 객체 생성
 var router = express.Router();
@@ -46,7 +63,7 @@ router.route('/process/login').post(function(req, res) {
   res.writeHead(200, {
     'Content-Type': 'text/html;charset=utf8'
   });
-  res.write('<form method="post" action="/process/register"><h2>게시물 등록</h2>');
+  res.write('<form method="post" enctype="multipart/form-data" action="/process/register"><h2>게시물 등록</h2>');
   res.write('<table border="1px solid black" width="500">');
   res.write('<tr><td>ID</td><td>' + req.session.user.id + '</td></tr>');
   res.write('<tr><td>제목</td><td><input type="text" name="title" size="40"></td></tr>');
@@ -56,10 +73,25 @@ router.route('/process/login').post(function(req, res) {
   res.end();
 });
 
-router.route('/process/register').post(function(req, res) {
+router.route('/process/register').post(upload.array('image', 1), function(req, res) {
   var paramId = req.session.user.id;
   var paramTitle = req.body.title;
   var paramContent = req.body.content;
+
+  try {
+    var file = req.files;
+
+    var originalname = file[0].originalname;
+    var mimetype = file[0].mimetype;
+    var size = file[0].size;
+
+    console.log('Original Name : ' + originalname);
+    console.log('MimeType : ' + mimetype);
+    console.log('Size : ' + size);
+
+  } catch (error) {
+    console.dir(error.stack);
+  }
 
   res.writeHead(200, {
     'Content-Type': 'text/html;charset=utf8'
@@ -69,7 +101,7 @@ router.route('/process/register').post(function(req, res) {
   res.write('<tr><td width=80>ID</td><td>' + req.session.user.id + '</td></tr>');
   res.write('<tr><td>제목</td><td>' + paramTitle + '</td></tr>');
   res.write('<tr><td height=80>내용</td><td>' + paramContent + '</td></tr>');
-  res.write('<tr><td>첨부파일</td><td>' + '</td></tr></table>');
+  res.write('<tr><td>첨부파일</td><td>' + originalname + '</td></tr></table>');
   res.end();
 });
 
