449aa96e4b6f827d38f80260ee3bd2c3fabd1872
jooyeong <young951109@naver.com>
2019-02-12 15:41:32 +0900

    post 방식을 이용한 데이터 전달

--- Node.js/OpenTutorials/app.js
+++ Node.js/OpenTutorials/app.js
@@ -3,6 +3,10 @@
 //express 모듈 추출
 var express = require('express');
 
+//express 모듈이 기본적으로 post 방식을 지원하지 않기 때문에
+//post 방식으로 데이터를 받기 위해서는 bodyParser가 꼭 필요하다!!
+var bodyParser = require('body-parser');
+
 //express 모듈은 함수이고 application을 리턴한다.
 var app = express();
 app.locals.pretty = true; //jade 파일의 코드를 정리해줌
@@ -10,21 +14,43 @@ app.set('view engine', 'jade'); //jade 템플릿 엔진 설정
 app.set('views', './views');
 
 app.use(express.static('public')); //정적인 파일이 위치할 디렉토리를 지정 (public 디렉토리로 지정함)
+app.use(bodyParser.urlencoded({ extended: false })) //bodyParser 연결
 
 //app.get() : 라우터, 라우팅 역할을 함!
-app.get('/topic', function(req, res) {
+app.get('/form', function(req, res) {
+  res.render('form');
+});
+
+app.get('/form_receiver', function(req, res) {
+  var title = req.query.title;
+  var description = req.query.description;
+  res.send(title + ", " + description);
+});
+
+app.post('/form_receiver', function(req, res) {
+  var title = req.body.title;
+  var description = req.body.description;
+  res.send(title + ', ' + description);
+});
+
+app.get('/topic/:id', function(req, res) {
   var topics = [
     'Javascript is ...',
     'Nodejs is ...',
     'Express is ...'
   ];
   var output = `
-    <a href='/topic?id=0'>JavaScript</a><br>
-    <a href='/topic?id=1'>Nodejs</a><br>
-    <a href='/topic?id=2'>Express</a><br><br>
-    ${topics[req.query.id]}
-  `
+    <a href='/topic/0'>JavaScript</a><br>
+    <a href='/topic/1'>Nodejs</a><br>
+    <a href='/topic/2'>Express</a><br><br>
+    ${topics[req.params.id]}
+  ` //semantic url 방식으로 매개변수를 받고 싶다면 req.query가 아니라 req.params로 받으면 됨.
+
   res.send(output);
+});
+
+app.get('/topic/:id/:mode', function(req, res) {
+  res.send(req.params.id + ", " + req.params.mode);
 });
 
 app.get('/template', function(req, res) {
